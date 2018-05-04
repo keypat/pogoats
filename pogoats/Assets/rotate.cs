63486973896f97f0d3525cb88e5816a99d86c3cb
Keyur Patel <patelk36@mcmaster.ca>
2018-05-04 15:55:23 -0400

    added rotate scene

---
+++ pogoats/Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+	public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+		rotationSpeed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationX = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
+		transform.RotateAround(Vector3.up, -rotationX);
+	}
+}
